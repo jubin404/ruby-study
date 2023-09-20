1b0d1701ce6e0c8998d22e673162bb7c0ffc0a59
jubin404 <jubin.jose@terrificminds.com>
2023-09-20 12:28:58 +0530

    initial commit

---
+++ conditional_statements.rb
@@ -0,0 +1,33 @@
+puts 'Enter name'
+name = gets.chomp
+
+puts 'Enter age'
+age = gets.chomp.to_i
+
+puts 'Enter gender (m/f) [optional]'
+gender = gets.chomp.downcase
+
+#switch
+case age
+	when 0..10
+		$category = 'child'
+	when 11..19
+		$category = 'teenager'
+	else 
+		$category = 'adult'
+end
+
+#if else
+if gender == 'm'
+	gender = ' male'
+elsif gender == 'f'
+	gender = ' female'
+else
+	gender = ''
+end
+
+#unless
+puts "#{name} is a #{age} year old#{gender unless gender.empty?} #{$category}"
+
+#ternary
+puts "#{name} is a #{age < 18 ? 'minor' : 'major'}"
