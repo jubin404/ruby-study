9a99d08ffe739ae3f79991ed8f8412a2310f5438
jubin404 <jubin.jose@terrificminds.com>
2023-09-28 18:03:31 +0530

    add regex for email, url and phone numbers

--- regex.rb
+++ regex.rb
@@ -1,4 +1,7 @@
 sentence = 'my name is jubin jose 34545 jose'
+email = 'jubin.jose@terrificminds.com'
+phone = '+91 8374375863'
+url = 'https://www.terrificminds.com'
 
 #get index of first match
 puts /jubin/ =~ sentence
@@ -17,3 +20,12 @@ puts sentence.sub(/jose/,'jeevan')
 
 #replace all matches
 puts sentence.gsub(/jose/,'jeevan')
+
+#email validation
+puts (/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i).match?(email)
+
+#phone number validation
+puts (/\A(?:\+?\d{1,3}\s*-?)?\(?(?:\d{3})?\)?[- ]?\d{3}[- ]?\d{4}\z/).match?(phone)
+
+#uri validation
+puts (/^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix).match?(url)
